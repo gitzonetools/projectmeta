ebade5e3dcb8ccadb78f137c616629366397633c
Phil Kunz <gitlab@philkunz.com>
2018-03-09 16:01:12 +0100

    update to latest standards

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,8 @@
-import { expect, tap } from 'tapbundle'
-import * as metaproject from '../ts/index'
+import { expect, tap } from 'tapbundle';
+import * as metaproject from '../ts/index';
 
 tap.test('first test', async () => {
-  console.log(metaproject.standardExport)
-})
+  console.log(metaproject.standardExport);
+});
 
-tap.start()
+tap.start();
